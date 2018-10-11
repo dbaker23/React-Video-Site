852a920dbcf0185420039e4387556691a985d52a
Derek <derekjohnbaker23@gmail.com>
2018-10-11 15:36:45 -0500

    Added youtube API key and input bar.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { SearchBar } from './SearchBar';
+import YTSearch from 'youtube-api-search';
 
-class App extends Component {
+const API_KEY = 'AIzaSyD43JDNPlyD6Ta4mKHTvN3b3OolmGY3QGk';
+
+YTSearch(
+  {key: API_KEY, term: 'surboards'}, 
+  function(data) { console.log(data) }
+);
+
+export class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div style={{textAlign:"center"}}>
         <h1>Hello World!</h1>
+        <SearchBar />
       </div>
     );
   }
 }
-
-export default App;
