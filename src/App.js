243167255a58e360f1d4559453569e4338432056
Derek <derekjohnbaker23@gmail.com>
2018-10-12 11:21:10 -0500

    Search list implemented. Video details fleshed out.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
-import { SearchBar } from './SearchBar';
+import { SearchBar } from './components/SearchBar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/VideoList';
 
 const API_KEY = 'AIzaSyD43JDNPlyD6Ta4mKHTvN3b3OolmGY3QGk';
 
-YTSearch(
-  {key: API_KEY, term: 'surboards'}, 
-  function(data) { console.log(data) }
-);
-
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surboards' }, ( videos ) => { 
+      this.setState({ videos });
+    });
+    console.log(this.state.videos);
+  }
   render() {
     return (
       <div style={{textAlign:"center"}}>
         <h1>Hello World!</h1>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
