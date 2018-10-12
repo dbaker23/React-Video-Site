fa4c77758647cf52b1b49657d30d344c0d2f4235
Derek <derekjohnbaker23@gmail.com>
2018-10-12 11:26:52 -0500

    Video details show with working video player.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { SearchBar } from './components/SearchBar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
 
 const API_KEY = 'AIzaSyD43JDNPlyD6Ta4mKHTvN3b3OolmGY3QGk';
 
@@ -11,7 +12,7 @@ export class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: 'surboards' }, ( videos ) => { 
+    YTSearch({ key: API_KEY, term: 'starcraft 2' }, ( videos ) => { 
       this.setState({ videos });
     });
     console.log(this.state.videos);
@@ -21,6 +22,7 @@ export class App extends Component {
       <div style={{textAlign:"center"}}>
         <h1>Hello World!</h1>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
